34103990ebad9f9746d11c0590ed04c857039783
Vatsal Sanchala <98870768+bevatsal1122@users.noreply.github.com>
2022-03-14 15:26:44 +0530

    Update IncorrectAddress.jsx

--- imports/ui/IncorrectAddress.jsx
+++ imports/ui/IncorrectAddress.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 export const IncorrectAddress = () => (
   <div id="all-inc-add">
-    <h1>404! Server Not Found...</h1>
+    <h1>404 Server Not Found !!</h1>
       <h2>
-          Database refused to connect<br />
+          Database Refused to Connect...<br />
           System Rendering Failed...
       </h2>
     <ul>
       <li>Check for the Correct Location</li>
-      <li>Check Your Connection</li>
-      <li>Check The Proxy and The Firewall</li>
+      <li>Check your Connection</li>
+      <li>Check the Proxy and the Firewall</li>
     </ul>
     <h3><span>Go to <a href={window.location.href.slice(0, 22)}>Register Page</a></span><br />
-    <span>Go to <a href={window.location.href.slice(0, 22) + "./login"}>Login Page</a></span></h3>
+    <span>Go to <a href={window.location.href.slice(0, 22) + "login"}>Login Page</a></span></h3>
   </div>
 );
